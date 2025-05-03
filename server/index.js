edfb47af4e00c7033f4c3e9c4afffd0fe793cebf
Konstantinos Nikas <132261852+KonstantinosNikas@users.noreply.github.com>
2025-05-03 22:30:30 +0300

    Update index.js parse int

--- server/index.js
+++ server/index.js
@@ -18,7 +18,8 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT, 10),
+,
   ssl: {
     rejectUnauthorized: false, // needed for Render's self-signed cert
   }
